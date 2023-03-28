6ac077c9baee103c24f67d8f9ed73cf73959aa85
Audric Rosier <audricrosier@gmail.com>
2023-03-28 19:10:54 +0900

    js edit

--- public/js/save.js
+++ public/js/save.js
@@ -1,64 +1,64 @@
 // transform Local Storage into array
 function saveInArray() {
-	console.log("saveInArray");
+	console.log("saveInArray")
 	if (localStorage !== null) {
-		const ls = Object.entries(localStorage);
-		return ls;
+		const ls = Object.entries(localStorage)
+		return ls
 	}
 
-	return false;
+	return false
 }
 
 // Delete all cookies
 function deleteAllCookies() {
-	localStorage.clear();
+	localStorage.clear()
 }
 
 // Create a backup in the local storage
 function save(name, value) {
-	localStorage.setItem(name, value);
+	localStorage.setItem(name, value)
 }
 
 // Retrieve the backup in the local storage
 function getSave() {
-	let save = saveInArray();
+	let save = saveInArray()
 
 	if (save) {
 		if (save.length === 0) {
-            document.getElementById("hiragana").checked = true;
-            selectDificulty("hiragana");
+            document.getElementById("hiragana").checked = true
+            selectDificulty("hiragana")
 		} else {
 			save.forEach((element) => {
-				console.log(element);
+				console.log(element)
 
 				switch (element[0]) {
 					case "dificulty":
-						element = element[1];
+						element = element[1]
 
 						if (element != "") {
-							let split = element.split(",");
-							difTable = split;
+							let split = element.split(",")
+							difTable = split
 
 							difTable.forEach((element) => {
-								document.getElementById(element).checked = true;
-								selectDificulty(element);
-							});
+								document.getElementById(element).checked = true
+								selectDificulty(element)
+							})
 						} else {
-							document.getElementById("hiragana").checked = true;
-							selectDificulty("hiragana");
+							document.getElementById("hiragana").checked = true
+							selectDificulty("hiragana")
 						}
-						break;
+						break
 
 					case "theme":
-						NextTheme = element[1];
-						selectTheme(NextTheme);
-						break;
+						NextTheme = element[1]
+						selectTheme(NextTheme)
+						break
 
 					default:
-						// console.log("default");
-						break;
+						// console.log("default")
+						break
 				}
-			});
+			})
 		}
 	}
 }
@@ -66,12 +66,12 @@ function getSave() {
 // Function to create the cookie
 function createCookie(name, value, days) {
 	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-		expires = " expires=" + date.toGMTString();
+		var date = new Date()
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+		expires = " expires=" + date.toGMTString()
 	} else {
-		expires = "";
+		expires = ""
 	}
 
-	document.cookie = escape(name) + "=" + escape(value) + expires + " path=/";
+	document.cookie = escape(name) + "=" + escape(value) + expires + " path=/"
 }
