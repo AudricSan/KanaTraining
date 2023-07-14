6f252e38003ab21392f015b22313792352750eb0
AudricSan <audricrosier@gmail.com>
2023-07-14 22:57:08 +0200

    Create const.js

---
+++ public/js/const.js
@@ -0,0 +1,30 @@
+let dificulty = [];
+const Hiragana = new kana();
+let character = document.querySelector('.character');
+
+
+
+
+
+
+
+
+
+
+
+
+// let input = document.querySelector('.input');
+// let NextTheme;
+// let selectedCharacter;
+
+// let myPoint = document.querySelector('.good');
+// let totalPoint = document.querySelector('.total');
+
+// const toast = document.getElementById("notification");
+// const helpspawner = document.getElementById("help-spawner");
+// const helpContainer = document.getElementById("help");
+// const closer = document.querySelector('.helpcloser');
+// const helpbtn = document.querySelector('.help');
+
+// const twitter = document.querySelector('.twitter-share');
+// const delay = 1000;
