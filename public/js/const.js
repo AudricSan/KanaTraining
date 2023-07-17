1fa40959cf990a2c4f7f7b63fca80f365f154707
AudricSan <audricrosier@gmail.com>
2023-07-17 20:45:47 +0200

    Update const.js

--- public/js/const.js
+++ public/js/const.js
@@ -2,6 +2,14 @@ let dificulty = [];
 const Hiragana = new kana();
 let character = document.querySelector('.character');
 
+const toast = document.getElementById("toast");
+const delay = 1000;
+
+let input = document.querySelector('.input');
+let myPoint = document.querySelector('.good');
+let totalPoint = document.querySelector('.total');
+
+const twitter = document.querySelector('.twitter-share');
 
 
 
@@ -9,22 +17,7 @@ let character = document.querySelector('.character');
 
 
 
-
-
-
-
-// let input = document.querySelector('.input');
-// let NextTheme;
-// let selectedCharacter;
-
-// let myPoint = document.querySelector('.good');
-// let totalPoint = document.querySelector('.total');
-
-// const toast = document.getElementById("notification");
 // const helpspawner = document.getElementById("help-spawner");
 // const helpContainer = document.getElementById("help");
 // const closer = document.querySelector('.helpcloser');
 // const helpbtn = document.querySelector('.help');
-
-// const twitter = document.querySelector('.twitter-share');
-// const delay = 1000;
