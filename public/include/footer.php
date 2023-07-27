7adbf766b0bf72060fa2fed729c07f253c9d97aa
AudricSan <audricrosier@gmail.com>
2023-07-27 10:06:51 +0200

    Create footer.php

---
+++ public/include/footer.php
@@ -0,0 +1,32 @@
+<?php
+echo "
+    <footer>
+            <a href='' target='_blank' class='twitter-share'>
+                <i class='fa fa-twitter'> </i>
+                <span>Partager sur Twitter</span>
+            </a>
+
+            <div class='author'>
+                <a href='https://twitter.com/AudricSan' target='_blank'>
+                    <span class='fa-solid fa-user'></span>
+                    par Audric_San
+                </a>
+
+                <p>|</p>
+
+                <a href='https://github.com/AudricSan/KanaTraining' target='_blank' class='git'>
+                    Le projet sur GitHub <span class='fab fa-github icongit'></span>
+                </a>
+            </div>
+
+            <!-- Script Block -->
+            <script src='js/theme.js'></script>
+            <script src='js/kana.js'></script>
+            <script src='js/const.js'></script>
+            <script src='js/save.js'></script>
+            <script src='js/function.js'></script>
+        </footer>
+    </body>
+
+    </html>
+";
